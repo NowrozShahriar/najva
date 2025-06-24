b24feca7a248c737ab4a1391d8ceb67a8b3383c0
NowrozShahriar <nowroz.shahriar@gmail.com>
2025-06-24 09:58:03 +0600

    Initial Commit

---
+++ lib/najva_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule NajvaWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use NajvaWeb, :html
+
+  embed_templates "page_html/*"
+end
