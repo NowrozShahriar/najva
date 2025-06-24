512421addfae6cec1066cc2af0c25b6b2aa2ccdf
NowrozShahriar <nowroz.shahriar@gmail.com>
2025-06-25 03:32:46 +0600

    code migrate

---
+++ lib/najva_web/components/components.ex
@@ -0,0 +1,20 @@
+defmodule NajvaWeb.Components do
+  use Phoenix.Component
+
+  # This component is used to hide the list pane on small screens.
+  # It can be included in any LiveView or HTML template where you want to control the visibility of the list pane.
+  attr :hide_class, :string, required: true
+  attr :width, :string, default: "768px"
+
+  def visibility(assigns) do
+    ~H"""
+    <style>
+      @media (max-width: <%= @width %>) {
+        .<%= @hide_class %> {
+          display: none;
+        }
+      }
+    </style>
+    """
+  end
+end
